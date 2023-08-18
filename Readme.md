4ef5f7df1c7d279f749b4c7a54d30cd4218f12db
ABINASH-PARHI <abinashparhi14@gmail.com>
2023-08-18 20:28:45 +0530

    Changed

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# YouTube Video Downloader
+This a basic project for downloading Videos in the computer's local storage and give an basic sentiment analysis
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-# This is a sample Python script.
 import pyttsx3
-
-sound = pyttsx3.init()
 import tkinter as tk
 from pytube import YouTube
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+
+nltk.download('vader_lexicon')
+
+sound = pyttsx3.init()
+
+sia = SentimentIntensityAnalyzer()
+
+
+def analyze_sentiment(text):
+    sentiment_scores = sia.polarity_scores(text)
+    compound_score = sentiment_scores['compound']
+    if compound_score >= 0.05:
+        return "Positive"
+    elif compound_score <= -0.05:
+        return "Negative"
+    else:
+        return "Neutral"
 
 
 def on_progress(stream, chunk, bytes_remaining):
@@ -19,26 +35,34 @@ def startDownload():
         ytlink = link.get()
         ytobject = YouTube(ytlink, on_progress_callback=on_progress)
         video = ytobject.streams.get_highest_resolution()
+
         sound.say("Download Started. Please Wait.")
         sound.runAndWait()
+
+        title_sentiment = analyze_sentiment(ytobject.title)
+
         video_title.set("Title: " + ytobject.title)
         video_channel.set("Channel: " + ytobject.author)
+        sentiment_label.config(text="Sentiment: " + title_sentiment)
+
         video.download()
         download_status_label.config(text="Download Complete!")
         sound.say("Download Complete!")
         sound.runAndWait()
+
     except:
-        sound.say("Invalid link. Check link again.")
+        sound.say("Invalid link or an error occurred.")
         sound.runAndWait()
-        download_status_label.config(text="Invalid link. Check link again")
+        download_status_label.config(text="Invalid link or an error occurred.")
 
 
 root = tk.Tk()
 root.geometry("1366x768")
 root.title("YouTube Video Downloader")
 
-title = tk.Label(root, text="Insert a YouTube link", font=("Roboto", 26))
-title.pack(padx=10, pady=10)
+
+title_label = tk.Label(root, text="Insert a YouTube link", font=("Roboto", 26))
+title_label.pack(padx=10, pady=10)
 
 url = tk.StringVar()
 link = tk.Entry(root, width=400, textvariable=url)
@@ -53,14 +77,17 @@ title_label.pack()
 channel_label = tk.Label(root, textvariable=video_channel, font=("Roboto", 18, "bold"))
 channel_label.pack()
 
-note = tk.Label(root, text="Note:- The downloaded YouTube video will be saved in the same directory as the program.",
-                font=("Roboto", 18))
+note = tk.Label(root, text="Note:- The downloaded YouTube video will be saved in the same directory as the program.", font=("Roboto", 18))
 note.pack(padx=10, pady=10)
 note = tk.Label(root, text="Note:- Please make sure that you are connected to the internet.", font=("Roboto", 18))
 note.pack(padx=10, pady=10)
 
+
 button = tk.Button(root, text="Download Video", command=startDownload)
 button.pack(padx=12, pady=10)
+
+sentiment_label = tk.Label(root, text="", font=("Roboto", 16))
+sentiment_label.pack()
 
 download_status_label = tk.Label(root, text="", font=("Roboto", 16))
 download_status_label.pack()
